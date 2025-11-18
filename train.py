5f7a7e6186a255ff74fc426ede6206930a8451ac
rozette-hesse <rozettehesse@inbalance.ai>
2025-11-18 18:35:57 +0200

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ train_x_synth, train_y_synth, _, _ = load_synthetic_data("synthetic_data.txt")
 periods_real = read_period_file("calendar.txt")
 train_x_real, train_y_real, test_x_real, test_y_real, last_known_period = make_train_test_sets(periods_real)
 
-# 3. Combine synthetic and real (use last N real examples)
+# 3. Combine synthetic and real data
 N_real = 78  # adjust if fewer real cycles
 train_x = np.array(train_x_synth.tolist() + train_x_real.tolist()[-N_real:])
 train_y = np.array(train_y_synth.tolist() + train_y_real.tolist()[-N_real:])
@@ -29,11 +29,11 @@ model.add(LSTM(100, activation="relu"))
 model.add(Dense(n_features))
 model.compile(optimizer="adam", loss="mse", metrics=[metrics.mae, metrics.mape])
 
-# 5. Train
+# 5. Train the model
 n_epochs = 4000
 model.fit(train_x, train_y, epochs=n_epochs, verbose=2)
 
-# 6. Evaluate on real test set
+# 6. Evaluate the model
 y_preds = model.predict(test_x_real, verbose=0)
 predictions = [[int(round(i[0])), int(round(i[1]))] for i in y_preds]
 acc = evaluate_predictions(test_y_real, predictions)
